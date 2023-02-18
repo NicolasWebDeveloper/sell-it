27a96528287b455bf310e1ed5d5854f5b53ea1a5
NicolasWebDeveloper <nicolas.duernbeck@gmx.at>
2023-02-18 23:22:17 +0100

    Edited success message

--- backend/controllers/authController.mjs
+++ backend/controllers/authController.mjs
@@ -6,5 +6,5 @@ export const createUser = catchAsync(async (req, res, next) => {
 
   const user = await User.create({ name, email, password, passwordConfirm });
   console.log(user);
-  res.status(200).json({ status: 'success', message: 'User created!' });
+  res.status(200).json({ status: 'success', statusCode: 200, message: 'User created' });
 });
