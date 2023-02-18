1d3295dc6a085b8310a25d4a09c45496b27f415f
NicolasWebDeveloper <nicolas.duernbeck@gmx.at>
2023-02-18 23:21:03 +0100

    Added error Handling for duplicated name

--- backend/controllers/errorController.mjs
+++ backend/controllers/errorController.mjs
@@ -1,1 +1,21 @@
-export default err => {};
+import config from '../config/config.mjs';
+
+const handleDuplicationError = () => {
+  return { status: 'fail', statusCode: 400, message: 'Name is already taken' };
+};
+
+export default (err, req, res, next) => {
+  let error = { ...err };
+  error.statusCode = error.statusCode || 500;
+  if (config.inProduction) {
+    //App is running in Production Mode. - Prevent Data - Leaking
+    if (error.code === 11000) error = handleDuplicationError();
+
+    res.status(error.statusCode).json(error);
+  } else {
+    console.error('ERROR! 🔥🔥🔥🔥🔥');
+    res.json(err);
+    console.log(err);
+    console.table(err);
+  }
+};
