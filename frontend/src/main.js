4f01666876ac07d73c7652d76bda06affd4ec624
NicolasWebDeveloper <nicolas.duernbeck@gmx.at>
2023-02-21 14:59:06 +0100

    Worked on the UI 🎨

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import router from './router';
 
 import BaseButton from './base/BaseButton.vue';
 
 const app = createApp(App);
+app.use(router);
 app.component('base-button', BaseButton);
 app.mount('#app');
